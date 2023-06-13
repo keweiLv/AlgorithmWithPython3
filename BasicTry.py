a76f830e7209d031719858613acf96569caf55d6
kezi <wow.kimsohyun.wu@gmail.com>
2023-06-13 23:39:48 +0800

    2023-06-13 daily

--- BasicTry.py
+++ BasicTry.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 
@@ -10,7 +11,21 @@ class Solution:
                 count += 1
         return count
 
+
 # 执行操作后的变量值
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         return sum(1 if op[1] == '+' else -1 for op in operations)
+
+
+# 数组中不等三元式的数目
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        n = len(nums)
+        ans = a = 0
+        for b in cnt.values():
+            c = n - a - b
+            ans += a * b * c
+            a += b
+        return ans
